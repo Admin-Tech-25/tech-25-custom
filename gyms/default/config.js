6450d7a4ea25eed2e43c25c543e1d1e41d70c65e
Admin-Tech-25 <admin@tech-25.com.au>
2026-05-15 01:02:49 +1000

    Create config.js

---
+++ gyms/default/config.js
@@ -0,0 +1,21 @@
+window.GYM_CONFIG = {
+    activeApp: "75custom",
+    name: "75 Custom",
+    shortName: "75 Custom",
+    logo: "/gyms/default/logo.webp", 
+    appIcon: "/gyms/default/icon.png",
+    color: "#2563EB", 
+    features: {
+        showLogo: false,
+        allowWaistTracking: true,
+        allowDevTools: true,
+        allowCustomRules: true
+    },
+    tasks: [
+        "Daily progress pic",
+        "45 minute workout",
+        "Read 10 pages of a book",
+        "45 minute outdoor workout",
+        "No cheat meal/alcohol"
+    ]
+};
